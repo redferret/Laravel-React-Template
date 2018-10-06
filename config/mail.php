89b666073d3773af2b521f3576792b2bcc341bf6
Richard DeSilvey <rdesilvey@gmail.com>
2018-10-05 19:36:02 -0600

    Mail config

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'rdesilvey@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS'),
         'name' => env('MAIL_FROM_NAME', 'Laravel-React-Template'),
     ],
 
