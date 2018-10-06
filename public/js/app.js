83e0e54e409f4aebf86908891fadaf03d944195a
Richard DeSilvey <rdesilvey@gmail.com>
2018-10-06 13:34:37 -0600

    Handle Actions that are not Registered yet

--- public/js/app.js
+++ public/js/app.js
@@ -1511,6 +1511,8 @@ var Actions = function () {
       var action = this._actions.get(payload.action);
       if (action instanceof Function) {
         action(payload);
+      } else if (action == undefined || action == null) {
+        console.error('Action \'' + payload.action + '\' is not registered');
       }
     }
   }, {
--- resources/assets/js/actions/AppActions.js
+++ resources/assets/js/actions/AppActions.js
@@ -10,6 +10,8 @@ class Actions {
     let action = this._actions.get(payload.action);
     if (action instanceof Function) {
       action(payload);
+    } else if (action == undefined || action == null) {
+      console.error(`Action '${payload.action}' is not registered`);
     }
   }
 
