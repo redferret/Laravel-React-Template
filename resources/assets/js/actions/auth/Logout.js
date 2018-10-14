05cdea6f17c57d093c861470cad07043c1eaceb0
Richard DeSilvey <rdesilvey@gmail.com>
2018-10-13 21:23:27 -0600

    Auth
    Beefed up the Logout action

--- resources/assets/js/actions/auth/Logout.js
+++ resources/assets/js/actions/auth/Logout.js
@@ -1,12 +1,13 @@
 import Actions from '../AppActions.js';
 import Axios from 'axios';
-import Router, { getCSRF } from '../../router.js';
+import Router, { checkStatus, handleError } from '../../router.js';
 
-import { LOG_OUT, LOG_IN } from '../../constants.js';
+import { LOG_OUT } from '../../constants.js';
 
 Actions.register(LOG_OUT, payload => {
   Axios(Router.request('POST', LOG_OUT))
+  .then(checkStatus)
   .then(response =>  {
     Router.relocateTo(response.request.responseURL);
-  });
+  }).catch(handleError);
 });
