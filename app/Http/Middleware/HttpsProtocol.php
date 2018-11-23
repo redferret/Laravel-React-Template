0c5e3f5cba42e7074dc2b1cb41b37723134dd4e4
Richard DeSilvey <rdesilvey@gmail.com>
2018-11-23 01:13:16 -0700

    Https Protocol

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+
+class HttpsProtocol {
+
+  public function handle($request, Closure $next) {
+    if (!$request->secure() && App::environment() === 'production') {
+      return redirect()->secure($request->getRequestUri(), 301);
+    }
+    return $next($request);
+  }
+}
